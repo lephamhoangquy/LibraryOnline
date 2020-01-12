08cc44aa30f7365bc055540b05869aaa015cb0c5
Lê Hoàng Sang <sanglh@ghn.vn>
2020-01-13 00:26:57 +0700

    upload store procedure find book by title

---
+++ Database/BookSerive_Procedure/sp_SearchBookByTitle.sql
@@ -0,0 +1,32 @@
+create proc sp_SearchBookByTitle
+	@TextToSearch nvarchar(100),
+	@offset int,
+	@limit int,
+	@total int output
+as
+begin
+	set @total = 0
+	begin tran
+	select @total=count(BookID) from Book where Book.Title like N'%'+@TextToSearch+'%'
+	if (@limit > 0)
+	begin
+		select B.*, C.CategoryName
+		from Book B join Category C on B.CategoryID=C.CategoryID
+		where B.Title like N'%'+@TextToSearch+'%'
+		order by B.BookID
+		OFFSET @offset ROWS FETCH NEXT @limit ROWS ONLY;
+	end
+	else
+	begin
+		select B.*, C.CategoryName
+		from Book B join Category C on B.CategoryID=C.CategoryID
+		where B.Title like N'%'+@TextToSearch+'%'
+		order by B.BookID
+		OFFSET @offset ROWS
+	end
+	commit tran
+	return
+end
+
+declare @total1 int
+exec sp_SearchBookByTitle N'facebook',0,0,@total1 output
