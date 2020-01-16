98add5c933271fafb993ca611d881ea7c67da12f
Lê Hoàng Sang <sanglh@ghn.vn>
2020-01-17 00:14:13 +0700

    update procedure create book

--- Database/BookSerive_Procedure/sp_InsertBook.sql
+++ Database/BookSerive_Procedure/sp_InsertBook.sql
@@ -1,13 +1,17 @@
+use BookService
+go
 create proc sp_InsertBook
 	@Author nvarchar(100),
 	@Title nvarchar(100),
 	@AboutBook nvarchar(max),
 	@Price float,
 	@Qty int,
-	@Base64String varchar(max)
+	@Base64String varchar(max),
+	@CreatedBookId int output
 as
 begin
 	declare @lastedBookID int
+	set @CreatedBookId = 0
 	begin tran
 		insert into Book(Author, Title, AboutBook, Price, Qty,CategoryID,IsDelete,CreatedAt)
 		values (@Author,@Title,@AboutBook,@Price,@Qty,1,0,GETDATE())
@@ -15,6 +19,8 @@ begin
 		select top 1 @lastedBookID=BookID
 		from Book
 		order by BookID desc
+
+		set @CreatedBookId = @lastedBookID
 
 		if (@Base64String <> '')
 		begin
